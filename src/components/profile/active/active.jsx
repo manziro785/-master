7c915249296ffcb2114f6e4830a5a31ff175083f
manziro785 <manziro785@gmail.com>
2024-03-18 09:26:40 +0600

    добавил несколько изменений

--- src/components/profile/active/active.jsx
+++ src/components/profile/active/active.jsx
@@ -3,6 +3,12 @@ import './activeModule.css'
 
 export default function Active() {
   return (
-    <></>
+    <>
+    <div className="active-box">
+      <div className="title-active-box">
+      Активность
+      </div>
+    </div>
+    </>
   )
 }
