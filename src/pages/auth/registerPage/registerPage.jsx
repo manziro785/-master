233aacf39b99b90073eb3d6d11d78c9c36ab0918
manziro785 <manziro785@gmail.com>
2024-03-29 12:59:42 +0600

    работа ч Ааь

--- src/pages/auth/registerPage/registerPage.jsx
+++ src/pages/auth/registerPage/registerPage.jsx
@@ -1,6 +1,7 @@
   import React, { useState } from 'react';
   import { Link } from 'react-router-dom';
   import {useForm} from "react-hook-form";
+  import './registerPageModule.css'
 
   const RegisterPage = () => {
     /*const [formData, setFormData] = useState({
